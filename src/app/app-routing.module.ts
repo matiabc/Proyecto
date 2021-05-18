329202cbd80d31421a9ab01e7f6a7102976e0948
Aaron <aaron.frenkel@iguanabee.com>
2021-05-18 17:54:21 -0400

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListaNotaComponent } from './lista-nota/lista-nota.component';
+import {VerNotaComponent} from './ver-nota/ver-nota.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'listanota', component: ListaNotaComponent},
+  {path: 'vernota', component: VerNotaComponent},
+  {path: '', component: VerNotaComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
