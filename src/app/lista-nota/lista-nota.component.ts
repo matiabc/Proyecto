cefc7952910abe16203080c5589969ca68176703
Aaron <aaron.frenkel@iguanabee.com>
2021-05-21 17:44:58 -0400

    Lista

--- src/app/lista-nota/lista-nota.component.ts
+++ src/app/lista-nota/lista-nota.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Nota } from '../notas';
 
 @Component({
   selector: 'app-lista-nota',
@@ -6,6 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./lista-nota.component.scss']
 })
 export class ListaNotaComponent implements OnInit {
+
+  lista:Array<Nota> = [
+    {titulo: "Hola", estado: "Abierto", descripcion: "lorem ipsum"},
+    {titulo: "Chao", estado: "Cerrado", descripcion: "lorem ipsum"},
+    {titulo: "Como estas?", estado: "En Proceso", descripcion: "lorem ipsum"},
+    {titulo: "Hola", estado: "Abierto", descripcion: "lorem ipsum"},
+    {titulo: "Chao", estado: "Cerrado", descripcion: "lorem ipsum"},
+    {titulo: "Como estas?", estado: "En Proceso", descripcion: "lorem ipsum"},
+    {titulo: "Hola", estado: "Abierto", descripcion: "lorem ipsum"},
+    {titulo: "Chao", estado: "Cerrado", descripcion: "lorem ipsum"},
+    {titulo: "Como estas?", estado: "En Proceso", descripcion: "lorem ipsum"}
+  ]; 
+
+  
 
   constructor() { }
 
